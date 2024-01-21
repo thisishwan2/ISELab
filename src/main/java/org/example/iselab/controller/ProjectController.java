bb97f14ef9b2d774870342678917848997f72b0a
thisishwan2 <feel000617@gmail.com>
2024-01-22 02:19:30 +0900

    fix: cors 수정

--- src/main/java/org/example/iselab/controller/ProjectController.java
+++ src/main/java/org/example/iselab/controller/ProjectController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.iselab.response.AllProjectResponse;
 import org.example.iselab.response.ProjectResponse;
 import org.example.iselab.service.ProjectService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("http://43.201.237.33:3000")
 public class ProjectController {
 
     private final ProjectService projectService;
