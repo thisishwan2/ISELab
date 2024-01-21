6f1cd7bc22fb8f4ba7aadd76525195cca4b6a5be
thisishwan2 <feel000617@gmail.com>
2024-01-22 02:34:00 +0900

    fix: cors

---
+++ src/main/java/org/example/iselab/CorsConfig.java
@@ -0,0 +1,30 @@
+package org.example.iselab;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://43.201.237.33/:3000");
+        config.addAllowedOrigin("http://localhost/:3000");
+        config.addAllowedHeader("*");
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.addExposedHeader("Authorization");
+        config.addExposedHeader("Authorization-refresh");
+        config.addExposedHeader("Set-Cookie");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
