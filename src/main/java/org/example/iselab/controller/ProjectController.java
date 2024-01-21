9e70b32438373bbe5f9ac09813a8d0d6baedccdb
thisishwan2 <feel000617@gmail.com>
2024-01-22 02:24:54 +0900

    fix: cors

--- src/main/java/org/example/iselab/controller/ProjectController.java
+++ src/main/java/org/example/iselab/controller/ProjectController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.example.iselab.response.AllProjectResponse;
 import org.example.iselab.response.ProjectResponse;
 import org.example.iselab.service.ProjectService;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,6 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin("http://43.201.237.33:3000")
 public class ProjectController {
 
     private final ProjectService projectService;
